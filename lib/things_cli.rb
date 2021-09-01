8be3e6093c399fe61a146a02bc9672013ba88ca7
andy-aguilar <andres.aguilar@flatironschool.com>
2021-09-01 09:25:04 -0400

    Update boilerplate code. Add pry functionality.

---
+++ lib/things_cli.rb
@@ -0,0 +1,22 @@
+def start_cli
+    puts "Hi! Welcome to the Things CLI"
+    main_menu
+    handle_user_choice
+    puts "Thanks for using the Things CLI!"
+  end
+  
+  def main_menu
+    puts "Here's what you can do:".cyan
+    puts "  1. Add Thing".cyan
+    puts "  2. List Things".cyan
+    # Add other options like "edit thing, delete thing, etc..."
+    puts "  exit".cyan
+    puts "Type the number corresponding to your choice, or 'exit' to leave the program".cyan
+  end
+
+  def handle_user_choice
+    input = gets.chomp
+    binding.pry
+    # add conditional logic for what to do when a user types a certain input
+  end
+
